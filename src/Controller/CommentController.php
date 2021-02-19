8d1a21a437bb8e493e52b4e8826e20b347158696
alioukahere <alioukahere@gmail.com>
2021-02-19 21:24:45 +0000

    (feat) delete comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class CommentController extends AbstractController
 {
@@ -54,8 +55,26 @@ class CommentController extends AbstractController
         return $this->redirect("{$target}#comments");
     }
 
+    /**
+     * @Route("/comment/{id}/delete", name="delete_comment")
+     */
     public function delete(Comment $comment)
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if ($this->getUser() === $comment->getAuthor() or $this->isGranted('ROLE_ADMIN')) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($comment);
+            $em->flush();
+
+            $target = $this->generateUrl(
+                'tutorial_view',
+                ['slug' => $comment->getTutorial()->getSlug()]
+            );
+
+            return $this->redirect("{$target}#comments");
+        }
+
+        throw new AccessDeniedException();
     }
 }
