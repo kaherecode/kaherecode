c51fbcedf3d542a776f3850b072baf8cfffaa9c5
alioukahere <alioukahere@gmail.com>
2021-03-14 11:03:15 +0000

    (style) build admin layout

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
