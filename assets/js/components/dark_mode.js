1cfecbe5ec0b3ecb4e4f3fea1bbef330f92fe6f1
Mamadou Aliou Diallo <alioukahere@gmail.com>
2021-04-30 09:59:36 +0000

    (style) use icons for dark mode toggler

--- assets/js/components/dark_mode.js
+++ assets/js/components/dark_mode.js
@@ -1,11 +1,26 @@
+const toggleIcon = (mode) => {
+  const suns = document.querySelectorAll('.dark-mode-switcher > svg.sun-icon')
+  const moons = document.querySelectorAll('.dark-mode-switcher > svg.moon-icon')
+
+  if (mode === 'dark') {
+    suns.forEach((item) => item.classList.remove('hidden'))
+    moons.forEach((item) => item.classList.add('hidden'))
+  } else {
+    moons.forEach((item) => item.classList.remove('hidden'))
+    suns.forEach((item) => item.classList.add('hidden'))
+  }
+}
+
 if (
   localStorage.kaherecode_theme === 'dark' ||
   (!('kaherecode_theme' in localStorage) &&
     window.matchMedia('(prefers-color-scheme: dark)').matches)
 ) {
   document.documentElement.classList.add('dark')
+  toggleIcon('dark')
 } else {
   document.documentElement.classList.remove('dark')
+  toggleIcon('light')
 }
 
 const darkModeSwitcher = document
@@ -15,29 +30,17 @@ const darkModeSwitcher = document
       if (!('kaherecode_theme' in localStorage)) {
         localStorage.kaherecode_theme = 'dark'
         document.documentElement.classList.add('dark')
-        updateSvgColor('dark')
+        toggleIcon('dark')
       } else {
         if (localStorage.kaherecode_theme === 'dark') {
           localStorage.kaherecode_theme = 'light'
           document.documentElement.classList.remove('dark')
-          updateSvgColor('light')
+          toggleIcon('light')
         } else {
           localStorage.kaherecode_theme = 'dark'
           document.documentElement.classList.add('dark')
-          updateSvgColor('dark')
+          toggleIcon('dark')
         }
       }
     })
   })
-
-const updateSvgColor = (mode) => {
-  document.querySelectorAll('.dark-mode-switcher > svg').forEach((item) => {
-    if (mode === 'dark') {
-      item.classList.remove('text-gray-400')
-      item.classList.add('text-red-400')
-    } else {
-      item.classList.remove('text-red-400')
-      item.classList.add('text-gray-400')
-    }
-  })
-}
