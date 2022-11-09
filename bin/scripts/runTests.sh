fa05849d2e2b0faccaa6a8ba2d95a7f5d6555cb4
alioukahere <alioukahere@gmail.com>
2022-11-09 23:29:52 +0000

    fix docker configs

---
+++ bin/scripts/runTests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+docker-compose down
+docker-compose up -d
+docker-compose exec -T php sh -c 'bin/console assets:install --no-interaction'
+docker-compose exec php sh -c 'bin/console doctrine:database:drop -f'
+docker-compose exec php sh -c 'bin/console doctrine:database:create'
+docker-compose exec php sh -c 'bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction'
+docker-compose exec php sh -c 'bin/console doctrine:fixtures:load --no-interaction'
+docker-compose exec php sh -c 'bin/console cache:clear'
+docker-compose exec php sh -c 'vendor/bin/simple-phpunit'
